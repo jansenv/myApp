87c6e9e6f7f5ff6195e327724e769f5e8fc19133
jansenv <jansenv44@gmail.com>
2020-02-27 15:59:18 -0600

    hello world

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace myApp {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+            Console.WriteLine ("The current time is " + DateTime.Now);
+        }
+    }
+}
